ad3a97aaf749d675c5036df7740756bd7ff591e8
harmonit <harmonit email>
2025-04-23 13:08:48 +0800

    1

--- Instant Messaging/src/common/message/user.go
+++
@@ -1,7 +0,0 @@
-package message
-type User struct {
-	UserId int `json:"userId"`//此操作保证序列化为此名称
-	UserPwd string `json:"userPwd"`
-	UserName string `json:"userName"`
-	UserStatus int`json:"userStatus"`
-}
--- Instant Messaging/src/server/model/user.go
+++
@@ -1,6 +0,0 @@
-package model
-type User struct {
-	UserId int `json:"userId"`//此操作保证序列化为此名称
-	UserPwd string `json:"userPwd"`
-	UserName string `json:"userName"`
-}
